063b0df0d434047d60359c8131fb74c259bb5d2a
tejal-supe <supetejalc@gmail.com>
2025-03-13 15:07:45 +0530

    Practised few basic array problem statements

---
+++ practice/array/easy.js
@@ -0,0 +1,39 @@
+// Write a function that takes an array of numbers and returns the sum of all elements.
+
+function sum(arr) {
+  //approach 1 - using for loop
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  // approach 2 - using reduce method of array
+  total = arr?.reduce((prev, curr) => {
+    console.log(prev, curr);
+    return prev + curr;
+  }, 0);
+  return total;
+}
+
+const nums = [4, 5, 7, 2, 6];
+console.log(sum(nums));
+
+// Write a function to find the maximum value in an array of numbers.
+
+function maxValue(arr) {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(maxValue(nums));
+
+// Write a function that checks if a given element exists in an array.
+
+function checkIfExists(arr, ele) {
+    return arr?.includes(ele)
+}
+console.log(checkIfExists(nums,9))
