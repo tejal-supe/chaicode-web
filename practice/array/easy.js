868cacae64547d0f61fac1d4750a6aeeb0676664
tejal-supe <supetejalc@gmail.com>
2025-03-13 15:53:31 +0530

    count occurance and remove duplicates

--- practice/array/easy.js
+++ practice/array/easy.js
@@ -15,7 +15,7 @@ function sum(arr) {
 }
 
 const nums = [4, 5, 7, 2, 6];
-console.log(sum(nums));
+// console.log(sum(nums));
 
 // Write a function to find the maximum value in an array of numbers.
 
@@ -29,11 +29,39 @@ function maxValue(arr) {
     return max;
 }
 
-console.log(maxValue(nums));
+// console.log(maxValue(nums));
 
 // Write a function that checks if a given element exists in an array.
 
 function checkIfExists(arr, ele) {
     return arr?.includes(ele)
 }
-console.log(checkIfExists(nums,9))
+// console.log(checkIfExists(nums, 9))
+
+// Write a function that counts how many times a specific element appears in an array.
+
+function countOccurance(arr,ele) {
+    let occurance = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === ele) {
+            occurance++;
+        }
+    }
+    return occurance;
+}
+
+let num1 = [3, 5, 6, 7, 3, 8, 3];
+// console.log(countOccurance(num1,5));
+
+// Write a function to remove duplicate elements from an array.
+
+function removeDuplicate(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++){
+        if (!newArray.includes(arr[i])) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+console.log(removeDuplicate(num1));
