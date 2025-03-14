e7cb2712aae9ffaa2a4912c632026bcb8cc2bb7f
tejal-supe <supetejalc@gmail.com>
2025-03-14 18:34:50 +0530

    Basic objects

---
+++ javascript/object.js
@@ -0,0 +1,20 @@
+//Create an object person with properties name, age, and city. Assign values to these properties and log the object to the console.
+
+const obj = {
+  name: "John Doe",
+  age: 30,
+  city: "New York",
+};
+console.log(obj);
+
+// Given an object car with properties make, model, and year, write a program to access and log each property.
+
+const carObject = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2022,
+};
+
+console.log("Make:", carObject.make); 
+console.log("Model:", carObject.model);
+console.log("Year:", carObject.year);  
